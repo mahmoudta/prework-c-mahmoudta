b53042d3db9217868d46586dbba1489d9c8ea5f4
mahmoud taya <mahmoud12597@gmail.com>
2019-12-20 15:56:38 +0200

    struct book

--- prework-c-mahmoudta/books.c
+++ prework-c-mahmoudta/books.c
@@ -1,1 +1,22 @@
 #include "books.h"
+#define NAME_LEN 50
+struct book_struct{
+    char name[NAME_LEN];
+    int number;
+    int promotion;
+    Zone zone;
+    bool is_borrowed;
+};
+Book Book_new(char name[],int number,int promotion,Zone zone) {
+  Book p = malloc(sizeof(*p));
+strcpy(p->name,name);
+  p->is_borrowed=false;
+  p->number = number;
+  p->promotion = promotion;
+  p->zone = zone;
+  return p;
+}
+char* get_zone_name(){
+    
+}
+
--- prework-c-mahmoudta/books.h
+++ prework-c-mahmoudta/books.h
@@ -2,5 +2,17 @@
 #define books_h
 
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+typedef enum {
+    KIDS,
+    HIGH_SCHOOL,
+    ADULT
+} Zone;
+typedef struct book_struct* Book;
+Book Book_new(char name[],int number,int promotion,Zone zone);
+char* getName(Book b);
+char* get_zone_name();
+/*Book* p1 = Book_new();*/
 #endif /* books_h */
